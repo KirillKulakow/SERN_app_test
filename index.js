32024d13e42ac04c2886a7dc1d8d99128cfadda8
KirillKulakow <kirillkulakow31@gmail.com>
2021-02-09 10:25:13 +0200

    fix

--- index.js
+++ index.js
@@ -9,11 +9,7 @@ const errorHandler = require("./middleware/ErrorHandlingMiddleware");
 const HTTP_PORT = process.env.PORT || 7200;
 
 const app = express();
-app.use(
-  cors({
-    origin: ["https://appcoinc.netlify.app", "http://localhost:3000"],
-  })
-);
+app.use(cors());
 app.use(express.json());
 app.use("/api", router);
 
@@ -21,7 +17,7 @@ app.use(errorHandler);
 
 app.use(express.static("client/build"));
 
-app.get("/*", (req, res) => {
+app.get("*", (req, res) => {
   res.sendFile(path.resolve(__dirname + "/client/build/index.html"));
 });
 
